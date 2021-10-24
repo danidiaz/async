55b4a8b1512172a760304eea9cf43c38e97564cc
Daniel Díaz <diaz_carrete@yahoo.com>
2021-10-24 12:50:31 +0200

    x2

--- test/test-async.hs
+++ test/test-async.hs
@@ -59,7 +59,7 @@ tests = [
       , testCase "concurrentlyE_left1" concurrentlyE_left1
       , testCase "concurrentlyE_left2" concurrentlyE_left2
       , testCase "concurrentlyE_earlyException" concurrentlyE_earlyException
-      , testCase "concurrentlyE_lateException" concurrentlyE_lateException
+--      , testCase "concurrentlyE_lateException" concurrentlyE_lateException
   ]
  ]
 
